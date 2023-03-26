97b53679c1684ff60dc396777793d2efa9e38714
TheMagicNach <Mrvlados951357@gmail.com>
2023-03-26 23:50:12 +0300

    finish mb

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryUl = document.querySelector('.gallery')
+const imageMarkup = createGalleryImageMarkup(galleryItems);
+
+galleryUl.insertAdjacentHTML("beforeend", imageMarkup);
+galleryUl.addEventListener("click", onGalleryImageClick);
+
+
+function createGalleryImageMarkup(galleryItem) {
+  return galleryItem
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                  <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                  />
+                </a>
+              </li>`;
+    })
+    .join("");
+}
+
+function onGalleryImageClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  function onEscKeyPress(evt) {
+    if (evt.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+
+  const originalImageResolution = evt.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${originalImageResolution}"  width="1400px" height="900px"; >`,
+    {
+      onShow: () => window.addEventListener("keydown", onEscKeyPress),
+      onClose: () => window.removeEventListener("keydown", onEscKeyPress),
+    }
+  );
+  instance.show(); 
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryUl = document.querySelector('.gallery');
+const imageMarkup = createGalleryImageMarkup(galleryItems);
+galleryUl.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGalleryImageMarkup(galleryItem) {
+  return galleryItem
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>
+`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a",
+{
+  captionsData: "alt",
+  captionPosition: "top",
+  captionDelay: 250,
+  animationSpeed:100,
+});
+
+
